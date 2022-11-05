8852883ded63fd804a71547e1674cb449b92e3d6
satinder <satindersingh472@gmail.com>
2022-11-04 21:50:03 -0600

    commenting_done_client_get

--- clients.py
+++ clients.py
@@ -5,19 +5,26 @@ from dbhelpers import conn_exe_close
 from apihelpers import verify_endpoints_info,upload_picture,bring_picture,send_email,add_for_patch
 
 
-
+# will grab the information about the client with token and client id
 def client_get():
+    # it will check for sent token and client id as headers
     invalid_headers = verify_endpoints_info(request.headers,['token','client_id'])
+    # if not sent then error will show up
     if(invalid_headers != None):
         return make_response(json.dumps(invalid_headers,default=str),400)
+    # will call the stored procedure with token and id as a parameters
     results = conn_exe_close('call client_get(?,?)',[request.headers['client_id'],request.headers['token']])
+    # response will have all the info about client matching id and token
     if(type(results) == list and len(results) == 1):
         return make_response(json.dumps(results[0],default=str),200)
+    # if not then error will show up
     elif(type(results) == list and len(results) != 1):
         return make_response(json.dumps('error getting user details',default=str),400)
     elif(type(results) != list ):
+        # if list is not a response then error with code 400 show up
         return make_response(json.dumps(results,default=str),400)
     else:
+        # if server error then 500 error code will show up
         return make_response(json.dumps(results,default=str),500)
 
 # client post will add a client to the database and at first it will just add first_name,last_name
