092a06c1190a220cf75ab51a8f27d6e738fb9ddd
satinder <satindersingh472@gmail.com>
2022-11-05 10:08:04 -0600

    client_logout_procedure_done

--- full_backup_file.sql
+++ full_backup_file.sql
@@ -108,7 +108,7 @@ CREATE TABLE `client_session` (
 
 LOCK TABLES `client_session` WRITE;
 /*!40000 ALTER TABLE `client_session` DISABLE KEYS */;
-INSERT INTO `client_session` VALUES (74,'3a8f37d790be414fb523188b189086ef',18,'2022-11-05 08:32:46'),(85,'djldjfldsjfldsajfjdalfj',18,'2022-11-05 08:38:20'),(86,'djldjfldsjfldsajfjda',18,'2022-11-05 08:38:30'),(87,'d65ea61cc4854a33ae732e1c487c0da8',18,'2022-11-05 09:16:36');
+INSERT INTO `client_session` VALUES (74,'3a8f37d790be414fb523188b189086ef',18,'2022-11-05 08:32:46');
 /*!40000 ALTER TABLE `client_session` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -464,4 +464,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-05  9:43:38
+-- Dump completed on 2022-11-05 10:07:51
