3626c839e296400756aebbb232ca285595f2de55
satinder <satindersingh472@gmail.com>
2022-11-12 08:00:23 -0700

    func_fixed_ride_post

--- ride.py
+++ ride.py
@@ -99,12 +99,12 @@ def ride_post():
     if(invalid_headers != None):
         return make_response(json.dumps(invalid_headers,default=str),400)
     # will check for required data is sent or not if not then it will ask for that
-    invalid = verify_endpoints_info(request.json,['from_city','to_city','travel_date','leave_time'])
+    invalid = verify_endpoints_info(request.json,['from_city','from_prov','to_city','to_prov','travel_date','leave_time'])
     if(invalid != None):
         return make_response(json.dumps(invalid,default=str),400)
     # after everythin sent it will call the stored procedure to post the ride 
-    results = conn_exe_close('call ride_post(?,?,?,?,?)',
-    [request.json['from_city'],request.json['to_city'],request.json['travel_date'],request.json['leave_time'],request.headers['token']])
+    results = conn_exe_close('call ride_post(?,?,?,?,?,?,?)',
+    [request.json['from_city'],request.json['from_prov'],request.json['to_city'],request.json['to_prov'],request.json['travel_date'],request.json['leave_time'],request.headers['token']])
     if(type(results) == list and results[0]['rides_posted_count'] == 1):
         # if ride posted successfully then it will give back the number of rides posted and ride id
         return make_response(json.dumps(results[0],default=str),200)
